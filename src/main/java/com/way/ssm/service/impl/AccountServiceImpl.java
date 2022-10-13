4fb97e1a7579960667c687027e888a1ea47790ef
luowei <luowei@qq.com>
2022-10-13 22:50:07 +0800

    v2

--- src/main/java/com/way/ssm/service/impl/AccountServiceImpl.java
+++ src/main/java/com/way/ssm/service/impl/AccountServiceImpl.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -16,34 +17,15 @@ import java.util.List;
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    @Autowired
+    private AccountMapper accountMapper;
     public List<Account> findAll() {
-        try {
-            InputStream is = Resources.getResourceAsStream("mybatis.xml");
-            SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-            SqlSession sqlSession = sessionFactory.openSession();
-            AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
             List<Account> list = accountMapper.findAll();
-            sqlSession.close();
             return list;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
-
 
     @Override
     public void save(Account account) {
-        try {
-            InputStream is = Resources.getResourceAsStream("mybatis.xml");
-            SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-            SqlSession sqlSession = sessionFactory.openSession();
-            AccountMapper accountMapper = sqlSession.getMapper(AccountMapper.class);
             accountMapper.save(account);
-            sqlSession.commit();
-            sqlSession.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 }
